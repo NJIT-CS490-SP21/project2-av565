b13890badc2f72af72f384b03f7b804662fdf021
EC2 Default User <ec2-user@ip-172-31-90-48.ec2.internal>
2021-02-27 17:15:16 +0000

    Add login functionality

--- src/App.js
+++ src/App.js
@@ -1,64 +1,31 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import { ListItem } from './ListItem.js';
-// import { useState, useRef, useEffect } from 'react';
-// import io from 'socket.io-client';
-
-// const socket = io(); // Connects to socket connection
-
-// function App() {
-//   const [messages, setMessages] = useState([]); // State variable, list of messages
-//   const inputRef = useRef(null); // Reference to <input> element
-
-//   function onClickButton() {
-//     if (inputRef != null) {
-//       const message = inputRef.current.value;
-//       // If your own client sends a message, we add it to the list of messages to 
-//       // render it on the UI.
-//       setMessages(prevMessages => [...prevMessages, message]);
-//       socket.emit('chat', { message: message });
-//     }
-//   }
-
-//   // The function inside useEffect is only run whenever any variable in the array
-//   // (passed as the second arg to useEffect) changes. Since this array is empty
-//   // here, then the function will only run once at the very beginning of mounting.
-//   useEffect(() => {
-//     // Listening for a chat event emitted by the server. If received, we
-//     // run the code in the function that is passed in as the second arg
-//     socket.on('chat', (data) => {
-//       console.log('Chat event received!');
-//       console.log(data);
-//       // If the server sends a message (on behalf of another client), then we
-//       // add it to the list of messages to render it on the UI.
-//       setMessages(prevMessages => [...prevMessages, data.message]);
-//     });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Chat Messages</h1>
-//       Enter message here: <input ref={inputRef} type="text" />
-//       <button onClick={onClickButton}>Send</button>
-//       <ul>
-//         {messages.map((item, index) => <ListItem key={index} name={item} />)}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import logo from './logo.svg';
 // import './App.css';
-import {FoodList} from './ListItem.js';
-import {SomeFunction} from './Board.js';
+import { FoodList } from './ListItem.js';
+import { SomeFunction, showBoard } from './Board.js';
 import './Board.css';
+import React, {useState} from 'react';
 
 
+// <p>Enter username: <input type = "text" required></input><button onClick = {showBoard}>Begin!</button></p>
 function App() {
+  const [username, setUsername] = useState('');
+  const check_input = () => {
+    if(document.getElementById("username").value == ""){
+      alert("Please enter a username!");
+      return;
+    }
+    document.getElementById("user_name_input").style.display = "none";
+    showBoard();
+  };
   return (
-    <SomeFunction/>
+    <div id = "MainApp">
+      <div id="user_name_input">
+        <label for = "username">Enter user-name: </label>
+        <input type="text" placeholder="username" id="username" name="username" onChangeText={(value) => setUsername(value)}/>
+        <input type="submit" value="Enter" onClick={check_input}/>
+      </div>
+      <SomeFunction/>
+    </div>
   );
 }
 
--- src/Board.js
+++ src/Board.js
@@ -5,34 +5,59 @@ import io from 'socket.io-client';
 
 const socket = io();
 
-Array.prototype.AbsLength = function() {
-    return this.filter(function(element) {
+Array.prototype.AbsLength = function () {
+    return this.filter(function (element) {
         return element != null;
     }).length;
 }
 
+Array.prototype.TicTacToeWinner = function () {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (this[a] && this[a] === this[b] && this[a] === this[c]) return this[a];
+    }
+    return null;
+}
+
+export function showBoard() {
+    document.getElementById("Board").style.display = "grid";
+}
 
 export function SomeFunction() {
     const [myList, changeList] = useState([]);
     const inputRef = useRef(null);
 
     function clicked(pos) {
-        if(myList.AbsLength() == 9) return;
+        if (myList.AbsLength() == 9) return;
         var newList = [...myList];
-        if(!newList[pos]) (newList.AbsLength() % 2) ? newList[pos] = "O" : newList[pos] = "X";
+        if (!newList[pos])(newList.AbsLength() % 2) ? newList[pos] = "O" : newList[pos] = "X";
         changeList(prevList => [...newList]);
         socket.emit('clicked', newList = newList);
+        const winner = newList.TicTacToeWinner();
+        winner ? console.log(winner, "won!") : console.log("No winner yet!");
     }
-    
+
     useEffect(() => {
-       socket.on('clicked', (data) =>{
-          console.log("Clicked:", data);
-          changeList(prevList => [...data]);
-       });
+        socket.on('clicked', (data) => {
+            console.log("Clicked:", data);
+            changeList(prevList => [...data]);
+            const winner = data.TicTacToeWinner();
+            winner ? console.log(winner, "won!") : console.log("No winner yet!");
+        });
     }, []);
 
     return (
-        <div class="board">
+        <div class="board" id="Board">
             <Square func={() => clicked(0)} pos='0' arr={myList}/>
             <Square func={() => clicked(1)} pos='1' arr={myList}/>
             <Square func={() => clicked(2)} pos='2' arr={myList}/>
