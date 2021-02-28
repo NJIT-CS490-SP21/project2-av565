5d22350949bd3cd054d314652fed678d4a0a99cf
EC2 Default User <ec2-user@ip-172-31-90-48.ec2.internal>
2021-02-28 02:50:28 +0000

    Fixed spectators and added reset button functionality

--- src/Board.js
+++ src/Board.js
@@ -3,6 +3,7 @@ import { useState, useRef, useEffect } from 'react';
 import { Square } from './Square.js';
 import io from 'socket.io-client';
 
+
 const socket = io();
 
 // Any prototypes FollowThisCase
@@ -25,7 +26,10 @@ Array.prototype.TicTacToeWinner = function () {
     ];
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
-        if (this[a] && this[a] === this[b] && this[a] === this[c]) return this[a];
+        if (this[a] && this[a] === this[b] && this[a] === this[c]){
+            document.getElementById("resetGame").style.display = "block";
+            return this[a] + " Won!";
+        }
     }
     return null;
 }
@@ -37,32 +41,49 @@ export function showBoard() {
 
 // Unless it is the main function, every functionIsInThisCase
 export function BigBoard(pp) {
+    const [who_won, set_who_won] = useState('');
     var users = pp.users;
-    const user = pp.current_user;
-    console.log(user);
+    const user = pp.current_user, [current_board, change_list] = useState([]), input_ref = useRef(null);
     socket.emit('username', users);
-    const [current_board, change_list] = useState([]);
-    const input_ref = useRef(null);
-
+    
     function clicked(pos) {
         if (current_board.AbsLength() == 9) return;
         var change_board = [...current_board];
-        if (!change_board[pos]){
-            if(change_board.AbsLength() % 2){
-                if(users[1] === user) change_board[pos] = "O";
+        if (!change_board[pos] && !change_board.TicTacToeWinner()) {
+            if (change_board.AbsLength() % 2) {
+                if (users[1] === user) change_board[pos] = "O";
                 change_list(prev => [...change_board]);
                 socket.emit('clicked', change_board = change_board);
             }
-            else if(users[0] === user){
+            else if (users[0] === user) {
                 change_board[pos] = "X";
                 change_list(prev => [...change_board]);
                 socket.emit('clicked', change_board = change_board);
             }
         }
-        const winner = change_board.TicTacToeWinner();
-        winner ? console.log(winner, "won!") : console.log("No winner yet!");
     }
 
+    function ShowGameInfo(info_pp) {
+        let x, y, spec, content;
+        if (info_pp.len >= 1) {
+            x = <p>Player X: {info_pp.lis[0]}</p>;
+            if (info_pp.curr == users[0]) x = [x, <label>(You)</label>];
+        }
+        if (info_pp.len >= 2) {
+            y = <p>Player O: {info_pp.lis[1]}</p>;
+            if (info_pp.curr == users[1]) y = [y, <label>(You)</label>];
+        }
+        if (info_pp.len >= 3) {
+            spec = <p>Spectators: {users.slice(2).join(", ")}</p>;
+        }
+        return [x, y, spec];
+    }
+    
+    function resetGame(){
+        change_list([]);
+        socket.emit('clicked', []);
+    }
+    
     useEffect(() => {
         socket.on('clicked', (data) => {
             change_list(prev => data);
@@ -73,7 +94,7 @@ export function BigBoard(pp) {
 
     return (
         <div id="MainGame">
-            <div id="GameInfo" class="GameInfo">
+            <div class="GameInfo" id="GameInfo">
                 <p id="Xname">Player X: {users[0]}</p>
                 <p id="Oname">Player O: {users[1]}</p>
                 <p id="Specname">Spectators: {users.slice(2).join(", ")}</p>
@@ -89,6 +110,8 @@ export function BigBoard(pp) {
                 <Square func={() => clicked(7)} pos='7' arr={current_board}/>
                 <Square func={() => clicked(8)} pos='8' arr={current_board}/>
             </div>
+            <h1>{(current_board.TicTacToeWinner()) ? current_board.TicTacToeWinner() : ""}</h1>
+            <button id="resetGame" class="resetGame" onClick={() => resetGame()}>Restart</button>
         </div>
     );
 }
